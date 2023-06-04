e7263016ba2734fae50aa5baecf379450af3d28c
Alex Finokhin <101180377+AlexFinokhin@users.noreply.github.com>
2023-06-04 04:49:19 +0300

    upd

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
-const rateLimit = require('express-rate-limit');
+
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-const authLimiterOptions = require('./configs/rateLimit');
-
-const { createUser, login } = require('./controllers/users');
-const { validationCreateUser, validationLogin } = require('./middlewares/validation');
 const auth = require('./middlewares/auth');
 const router = require('./routes/router');
 const errorHandler = require('./configs/errorHandler');
@@ -19,11 +15,7 @@ const { MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb', PORT = 3000 } = process
 
 const app = express();
 
-const authLimiter = rateLimit(authLimiterOptions);
-
 app.use(bodyParser.json());
-app.post('/signin', authLimiter, validationLogin, login);
-app.post('/signup', authLimiter, validationCreateUser, createUser);
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors());
@@ -31,7 +23,7 @@ app.use(auth);
 app.use(router);
 app.use(errors());
 
-app.use(errorHandler); // Use the errorHandler middleware
+app.use(errorHandler);
 
 async function start() {
   try {
--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,12 @@
-const router = require('express')
-  .Router();
-
+const router = require('express').Router();
 const cardsRouter = require('./cards');
 const usersRouter = require('./users');
+const { validationCreateUser, validationLogin } = require('../middlewares/validation');
+const { createUser, login } = require('../controllers/users');
 const NotFoundError = require('../errors/404-NotFoundError');
+
+router.post('/signin', validationLogin, login);
+router.post('/signup', validationCreateUser, createUser);
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
