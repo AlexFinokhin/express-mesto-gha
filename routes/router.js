25871188771f913d1904ba58a930c48384f97a75
Alex Finokhin <101180377+AlexFinokhin@users.noreply.github.com>
2023-06-04 04:58:19 +0300

    АПД

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,16 @@
-const router = require('express')
-  .Router();
-
+const router = require('express').Router();
+const { validationCreateUser, validationLogin } = require('../middlewares/validation');
+const { createUser, login } = require('../controllers/users');
 const cardsRouter = require('./cards');
 const usersRouter = require('./users');
 const NotFoundError = require('../errors/404-NotFoundError');
+
+const auth = require('../middlewares/auth'); // Добавлен импорт middleware auth
+
+router.post('/signin', validationLogin, login);
+router.post('/signup', validationCreateUser, createUser);
+
+router.use(auth); // Добавлен middleware auth перед роутами users и cards
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
