8affd9845df6d3fee0f6a9075eb4c4026eb9bb7b
Alex Finokhin <101180377+AlexFinokhin@users.noreply.github.com>
2023-06-04 04:06:12 +0300

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,19 +1,16 @@
 const router = require('express')
   .Router();
-const rateLimit = require('express-rate-limit');
 
 const { validationLogin, validationCreateUser } = require('../middlewares/validation');
 const { login, createUser } = require('../controllers/users');
-const authLimiterOptions = require('../configs/rateLimit');
 
 const cardsRouter = require('./cards');
 const usersRouter = require('./users');
 const NotFoundError = require('../errors/404-NotFoundError');
 
-const authLimiter = rateLimit(authLimiterOptions);
+router.post('/signin', validationLogin, login);
 
-router.post('/signin', authLimiter, validationLogin, login);
-router.post('/signup', authLimiter, validationCreateUser, createUser);
+router.post('/signup', validationCreateUser, createUser);
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
