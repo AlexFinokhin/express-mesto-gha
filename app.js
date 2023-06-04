cd22ed7b47773b196dc7d7ed8fa4b94a5b53fc14
Alex Finokhin <101180377+AlexFinokhin@users.noreply.github.com>
2023-06-04 04:51:28 +0300

    upd

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
-
+const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+const authLimiterOptions = require('./configs/rateLimit');
+
+const { createUser, login } = require('./controllers/users');
+const { validationCreateUser, validationLogin } = require('./middlewares/validation');
 const auth = require('./middlewares/auth');
 const router = require('./routes/router');
 const errorHandler = require('./configs/errorHandler');
@@ -15,7 +19,11 @@ const { MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb', PORT = 3000 } = process
 
 const app = express();
 
+const authLimiter = rateLimit(authLimiterOptions);
+
 app.use(bodyParser.json());
+app.post('/signin', authLimiter, validationLogin, login);
+app.post('/signup', authLimiter, validationCreateUser, createUser);
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors());
