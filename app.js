f26153f32db3c723c164b202f9cb9898870ea9e8
Alex Finokhin <101180377+AlexFinokhin@users.noreply.github.com>
2023-06-04 04:27:18 +0300

    upd

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+const authLimiterOptions = require('./configs/rateLimit');
 const logger = require('./configs/winston');
+const { createUser, login } = require('./controllers/users');
+const notFoundError = require('./errors/404-NotFoundError');
+const { validationCreateUser, validationLogin } = require('./middlewares/validation');
 const auth = require('./middlewares/auth');
 const router = require('./routes/router');
 
@@ -14,13 +19,17 @@ const { MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb', PORT = 3000 } = process
 
 const app = express();
 
-app.use(bodyParser.json());
+const authLimiter = rateLimit(authLimiterOptions);
 
+app.use(bodyParser.json());
+app.post('/signin', authLimiter, validationLogin, login);
+app.post('/signup', authLimiter, validationCreateUser, createUser);
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors());
 app.use(auth);
 app.use(router);
+app.use(errors());
 
 app.use((error, request, response, next) => {
   const { status = 500, message } = error;
@@ -36,6 +45,9 @@ async function start() {
     await mongoose.connect(MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+    });
+    app.use('*', (req, res) => {
+      res.status(notFoundError).send({ message: 'Такой страницы нет' });
     });
     await app.listen(PORT);
     console.log(`Ееееее все запустилось\n${MONGO_URL}\nPort: ${PORT}`);
--- routes/router.js
+++ routes/router.js
@@ -1,26 +1,14 @@
-const router = require('express').Router();
-const { errors } = require('celebrate');
-const rateLimit = require('express-rate-limit');
+const router = require('express')
+  .Router();
 
 const cardsRouter = require('./cards');
 const usersRouter = require('./users');
-
-const { validationLogin, validationCreateUser } = require('../middlewares/validation');
-const { login, createUser } = require('../controllers/users');
-
-const authLimiterOptions = require('../configs/rateLimit');
 const NotFoundError = require('../errors/404-NotFoundError');
-
-const authLimiter = rateLimit(authLimiterOptions);
-
-router.post('/signin', authLimiter, validationLogin, login);
-router.post('/signup', authLimiter, validationCreateUser, createUser);
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 router.use((request, response, next) => {
   next(new NotFoundError('Ошибка: Страница не найдена'));
 });
-router.use(errors());
 
 module.exports = router;
